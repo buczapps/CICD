3c91deb1040093f710e658c410ad2d70785ae819
Michal <trzydziech@gmail.com>
2025-04-26 11:41:15 +0200

    initial

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return('<h1>Hello BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrażanie!</p>')
+    return('<h1>Hello BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrażanie!</p><p>Wykonane o 11:40</p>')
 
 if __name__  == '__main__':
     application.run(debug=True)
